787ac32013dc1332016087c1f190d682bb6459f0
Eddy Martin <eddypmartin@gmail.com>
2023-02-03 10:40:52 -0500

    s2c2 ajout menu et wp_enqueue

--- functions.php
+++ functions.php
@@ -1,8 +1,22 @@
 <?php
 
+function ajouter_styles() {
 
-wp_enqueue_style(   'style-principale',
-                    get_template_directory_uri() . '/style.css',
-                    array(),
-                    filemtime(get_template_directory() . '/style.css')
-                );
+wp_enqueue_style(   'style-principale',  // identificateur du link css
+                    get_template_directory_uri() . '/style.css',  // enroit où se trouve le fichier style.css
+                    array(), // les fichiers css qui dépendent de style.css
+                    filemtime(get_template_directory() . '/style.css')  // version de notre style.css
+); 
+}
+add_action( 'wp_enqueue_scripts', 'ajouter_styles' );
+
+
+
+/* ----------------------------------- Enregistrement des menus */
+function enregistrement_nav_menu(){
+    register_nav_menus( array(
+        'principal' => 'Menu principal',
+        'footer'  => 'Menu pied de page'
+    ) );
+}
+add_action( 'after_setup_theme', 'enregistrement_nav_menu', 0 );         
