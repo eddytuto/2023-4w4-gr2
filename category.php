c5f22f7a32fe867d63e414f816b6b70dd46b91f9
Eddy Martin <eddypmartin@gmail.com>
2023-02-17 09:55:30 -0500

    s4c2  modèle category et affichage de bloc avec grid pour blocflex

--- category.php
+++ category.php
@@ -1,25 +1,23 @@
 <?php
 /**
- * Modèle par défaut
+ * Modèle category par défaut
  * 
  */
 ?>
 <?php get_header(); ?>
 <main>
-    <h3>front-page.php</h3>
+    <h3>category.php</h3>
     <section class="blocflex">
     <?php
         if (have_posts()): 
             while (have_posts()) : the_post(); ?>
             <article>
-                
-                <h3>
+                <h2>
                     <a href="<?php echo get_permalink();?>"><?php echo get_the_title(); ?></a>
-                </h3>
+                </h2>
                 <?php // the_content();  // affiche le contenu complet de l'article ?>
                 <?php // the_excerpt();   // affiche un résumé de l'article ?>
-        
-                <p><?= wp_trim_words(get_the_excerpt(), 10, "<span>&#10148;</span>") ?></p>
+                <p><?= wp_trim_words(get_the_excerpt(), 10, "&#10148;") ?></p>
             </article>   
             <?php endwhile;
         endif;    
--- readme.md
+++ readme.md
@@ -1,14 +1,12 @@
-# Exercice #1
+# Lab #3
 ## 4w4 - Conception d'interface et développement Web
 ### Auteur: Eddy Martin
 #### Description
-- Installation sur un serveur distant « siteground.com »
-- Développer les modules suivants:
-> header.php, front-page.php
-- Création de la branche exer1 
-- Compléter la codification sass
-- Améliorer le design global
+- Améliorer la classe .blocflex
+- Création du modèle category.php
+- Améliorer l'affichage des articles « markdown »
 
 #### Références
 - https://eddytuto.ca/
 - https://developer.wordpress.org/themes/
+- https:// ... cotre site syr site ground
