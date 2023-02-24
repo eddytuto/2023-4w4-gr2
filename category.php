4074c6042acf503c9a231875c1c453b5896d5942
Eddy Martin <eddypmartin@gmail.com>
2023-02-24 11:42:02 -0500

    s5c2 La classe WP_Query() dans category.php

--- category.php
+++ category.php
@@ -5,24 +5,28 @@
  */
 ?>
 <?php get_header(); ?>
-<main>
-    <h3>category.php</h3>
+<main class="site__main">
+    <code>category.php</code>
     <section class="blocflex">
-    <?php
-        if (have_posts()): 
-            while (have_posts()) : the_post(); ?>
+      <?php
+      $category = get_queried_object();
+      $args = array(
+         'category_name' => $category->slug,
+         'orderby' => 'title',
+         'order' => 'ASC'
+      );
+      $query = new WP_Query( $args );
+      if ( $query->have_posts() ) :
+         while ( $query->have_posts() ) : $query->the_post(); ?>
             <article>
-                <h2>
-                    <a href="<?php echo get_permalink();?>"><?php echo get_the_title(); ?></a>
-                </h2>
-                <?php // the_content();  // affiche le contenu complet de l'article ?>
-                <?php // the_excerpt();   // affiche un résumé de l'article ?>
-                <p><?= wp_trim_words(get_the_excerpt(), 10, "&#10148;") ?></p>
-            </article>   
-            <?php endwhile;
-        endif;    
-    ?>
-    </section>
+               <h2><a href="<?php the_permalink(); ?>"> <?= get_the_title(); ?></a></h2>
+               <p><?= wp_trim_words(get_the_excerpt(), 15) ?></p>
+            </article>
+         <?php endwhile; ?>
+      <?php endif;
+      wp_reset_postdata();?>
+   </section>
 </main>
+
 
 <?php get_footer(); ?>
