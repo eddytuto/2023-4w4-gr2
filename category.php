61343efe5edcf9917fd5a3d06ad16a673f5b57aa
Eddy Martin <eddypmartin@gmail.com>
2023-03-03 10:46:41 -0500

    s6c2 template-parts et galerie dans la page d'accueil

--- category.php
+++ category.php
@@ -18,17 +18,9 @@
       $query = new WP_Query( $args );
       if ( $query->have_posts() ) :
          while ( $query->have_posts() ) : $query->the_post(); ?>
-            <article>
-               <?php 
-               $titre = get_the_title();
-               if  ( $category->slug == 'cours')
-               {
-                  $titre = substr($titre,0,7);
-               }  
-               ?>   
-               <h2><a href="<?php the_permalink(); ?>"> <?= $titre ?></a></h2>
-               <p><?= wp_trim_words(get_the_excerpt(), 15) ?></p>
-            </article>
+         <?php
+         /* $category->slug peut avoir une de ces deux valeurs : "cours" ou "4w4" */
+          get_template_part('template-parts/categorie',$category->slug); ?>
          <?php endwhile; ?>
       <?php endif;
       wp_reset_postdata();?>
--- front-page.php
+++ front-page.php
@@ -10,18 +10,15 @@
     <section class="blocflex">
     <?php
         if (have_posts()): 
-            while (have_posts()) : the_post(); ?>
-            <article>
-                
-                <h3>
-                    <a href="<?php echo get_permalink();?>"><?php echo get_the_title(); ?></a>
-                </h3>
-                <?php // the_content();  // affiche le contenu complet de l'article ?>
-                <?php // the_excerpt();   // affiche un résumé de l'article ?>
-        
-                <p><?= wp_trim_words(get_the_excerpt(), 10, "<span>&#10148;</span>") ?></p>
-            </article>   
-            <?php endwhile;
+            while (have_posts()) : the_post(); 
+                if (in_category('galerie')){
+                 //get_template_part("template-parts/categorie", "galerie");
+                 the_content();
+                }
+                else {
+                    get_template_part("template-parts/categorie", "4w4");  
+                }
+             endwhile;
         endif;    
     ?>
     </section>
