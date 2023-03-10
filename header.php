e6b65e497aad00c50d6fa2cb7864e3b83fd85c68
Eddy Martin <eddypmartin@gmail.com>
2023-03-10 09:31:30 -0500

    s7c2 ajout de l'adaptabilité dans l'entete avec media query et input:checkbox

--- header.php
+++ header.php
@@ -6,18 +6,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
 </head>
-
+<?php 
+/*
+  $nouvelle_classe = ""
+  if (is_front_page()){
+    $nouvelle_classe = 'no-aside';
+  }
+*/
+?>
+<!-- body class="site  // $nouvelle_classe " -->
 <body class="site <?php echo (is_front_page()?'no-aside':'');?>">
+
    <header class="site__header">
     <section class="site__header__logo">
-        <div class="logomenu">
-            <?php the_custom_logo(); ?>
+      <?php the_custom_logo(); ?>
+      <div class="menu__recherche">
+            <input type="checkbox" id="chkMenu">
             <?php wp_nav_menu(array(
             "menu" => "entete",
             "container" => "nav"
             )) ?>
-        </div>
-        <?php  get_search_form(); ?>
+        
+            <?php  get_search_form(); ?>
+            <label class="burger" for="chkMenu"><img src="https://s2.svgbox.net/hero-outline.svg?ic=menu&color=000" width="20" height="20"></label>
+           
+      </div>  
     </section>
     <h1><a href="<?= bloginfo('url') ?>"><?= bloginfo('name') ?></a></h1>
     <h2><?= bloginfo('description') ?></h2>
